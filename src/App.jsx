1ad613bd45035563f220c8d4a23f505cd2cdde9d
valclassdevelop <developervalclass.com>
2024-12-16 11:08:46 +0700

    slicing

--- src/App.jsx
+++ src/App.jsx
@@ -312,6 +312,7 @@ const App = () => {
                     </div>
 
                     <div className="w-max h-max border border-black p-3 flex items-center justify-center text-[14px] mb-3 text-black">
+                        <SparklesIcon className="w-5 h-5 mr-2" />
                         <p>Make-up Artist - Best of 2025</p>
                     </div>
                     <h2 className="font-normal text-[40px] w-full">
@@ -402,6 +403,7 @@ const App = () => {
                 </div>
                 <div className="w-1/2 px-8 h-max">
                     <div className="w-max h-max border border-black p-3 flex items-center justify-center text-[14px] mb-3 text-black">
+                        <SparklesIcon className="w-5 h-5 mr-2" />
                         <p>Photo & Videography</p>
                     </div>
                     <h2 className="font-normal text-[40px]">
@@ -1104,12 +1106,12 @@ const App = () => {
                                     </li>
                                     <li className="list-disc">Catering</li>
                                 </ul>
-                                <div className="absolute h-[150px] border border-black w-1/3 right-0 bottom-4">
+                                <div className="absolute h-[150px] w-1/3 right-0 bottom-5">
                                     <ThreeDModel
                                         url={"../public/crown.glb"}
                                         intensity={3}
                                         speed={1}
-                                        scaleCustom={[10, 10, 10]}
+                                        scaleCustom={[16, 16, 16]}
                                     />
                                 </div>
                             </div>
